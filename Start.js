30f064c6534b98a1075c21918b004e315baea816
syndicate-goat-bot-azad <talukdershoikat@gmail.com>
2025-08-29 14:34:42 +0600

    Create Start.js

---
+++ Start.js
@@ -0,0 +1,24 @@
+/**
+ * @author NTKhang
+ * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
+ */
+
+const { spawn } = require("child_process");
+const log = require("./logger/log.js");
+
+function startProject() {
+	const child = spawn("node", ["Goat.js"], {
+		cwd: __dirname,
+		stdio: "inherit",
+		shell: true
+	});
+
+	child.on("close", (code) => {
+		if (code == 2) {
+			log.info("Restarting Project...");
+			startProject();
+		}
+	});
+}
+
+startProject();
